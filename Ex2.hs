9f3db2b3aa1737c8086e98a72f68cdb9da5d0b0c
JoyceChristine <70724350+JoyceChristine@users.noreply.github.com>
2020-09-03 21:55:57 -0300

    Delete Ex2.hs

--- Ex2.hs
+++
@@ -1,8 +0,0 @@
---Exercicio 2
-main  ::  IO ()
-main =  return  ()
-
-type Dist = (Float, Float)
-
-distancia :: Dist -> Dist -> Float
-distancia  (x2, x1) (y2, y1) = sqrt ( (x2 - x1) ^ 2 + (y2 - y1) ^ 2 )
